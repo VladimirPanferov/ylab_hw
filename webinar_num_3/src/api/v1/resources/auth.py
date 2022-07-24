031dd30f0ec5e700868e73dc367fa356dc622a1c
Vladimir Panferov <v3nseagal@gmail.com>
2022-07-25 01:10:40 +0700

    Update API Auth resource

--- webinar_num_3/src/api/v1/resources/auth.py
+++ webinar_num_3/src/api/v1/resources/auth.py
@@ -1,22 +1,44 @@
-from fastapi import APIRouter, Depends
+from fastapi import (
+    APIRouter,
+    Depends,
+)
 from fastapi.security import OAuth2PasswordRequestForm
 
-from api.v1.schemas.auth import (
+from src.services import (
+    AuthService,
+    get_current_user,
+)
+
+from ..schemas.auth import (
+    User,
     UserCreate,
     Token,
 )
 
 
-router = APIRouter(
-    prefix="/auth"
-)
+router = APIRouter()
 
 
 @router.post("/signup", response_model=Token)
-def singup(user_data: UserCreate):
-    pass
+def singup(
+    user_data: UserCreate,
+    service: AuthService = Depends(),
+):
+    return service.register_new_user(user_data)
 
 
 @router.post("/signin", response_model=Token)
-def signin(form_data: OAuth2PasswordRequestForm = Depends()):
-    pass
+def signin(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    service: AuthService = Depends(),
+):
+    return service.authenticate_user(
+        username=form_data.username,
+        password=form_data.password,
+    )
+
+
+@router.get("/user", response_model=User)
+def get_user(user: User = Depends(get_current_user)):
+    return user
+
